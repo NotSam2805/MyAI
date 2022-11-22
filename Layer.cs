d3588711dd90c6e12d2944fc9e5c92e9adf26ab1
NotSam2805 <98456028+NotSam2805@users.noreply.github.com>
2022-11-22 10:05:24 +0000

    Update Layer.cs
    more comments

--- Layer.cs
+++ Layer.cs
@@ -6,37 +6,41 @@ namespace MyAI
 {
     public class Layer
     {
-        private int nInputs;
+        private readonly int nInputs;//The size of input arrays that is expected
+        
+        //The inputs and outputs of this layer
         private double[] inputs;
         private double[] outputs;
-        public Neuron[] neurons;
+        
+        public Neuron[] neurons;//Cant remember why this is public, it feels like it shouldn't be
 
         public Layer(int numNeurons, int numInputs, double learnRate)
         {
+            //Set the number of inputs and the size of the array
             nInputs = numInputs;
             neurons = new Neuron[numNeurons];
 
             for(int i = 0; i < neurons.Length; i++)
             {
-                neurons[i] = new Neuron(numInputs, learnRate);
+                neurons[i] = new Neuron(numInputs, learnRate);//Make each neuron
             }
 
-            outputs = new double[numNeurons];
+            outputs = new double[numNeurons];//Don't know why I didn't put this at the top with the others, I wonder if it's lonely.
         }
 
         public void SetInputs(double[] input)
         {
-            if (input.Length != nInputs)
+            if (input.Length != nInputs)//Checks that the input is the right formatt
             {
                 //Console.WriteLine("Incorrect number of inputs for this layer, should be " + nInputs + " inputs, there is " + input.Count + " inputs");
                 return;
             }
 
-            inputs = input;
+            inputs = input;//Save this for debugging
 
             for(int i = 0; i < neurons.Length; i++)
             {
-                neurons[i].SetInputs(inputs);
+                neurons[i].SetInputs(inputs);//Set the input of each neuron
             }
         }
 
@@ -44,13 +48,13 @@ namespace MyAI
         {
             for(int i = 0; i < outputs.Length; i++)
             {
-                outputs[i] = neurons[i].CalcOutput();
+                outputs[i] = neurons[i].CalcOutput();//Get the output of each neuron
             }
 
             return outputs;
         }
 
-        public double[] CalcOutputHidden()
+        public double[] CalcOutputHidden()//Ignore this, I was trying something
         {
             for (int i = 0; i < outputs.Length; i++)
             {
@@ -60,7 +64,7 @@ namespace MyAI
             return outputs;
         }
 
-        public void ReWeightOutput(double[] desiredOutput, double[] output)
+        public void ReWeightOutput(double[] desiredOutput, double[] output)//Broken
         {
             for (int i = 0; i < neurons.Length; i++)
             {
@@ -68,7 +72,7 @@ namespace MyAI
             }
         }
 
-        public void ReWeightHidden(Layer prevLayer)
+        public void ReWeightHidden(Layer prevLayer)//Very broken
         {
             for (int i = 0; i < neurons.Length; i++)
             {
@@ -76,7 +80,7 @@ namespace MyAI
             }
         }
 
-        public void Mutate()
+        public void Mutate()//Read comment in NeuralNetwork.cs
         {
             for (int i = 0; i < neurons.Length; i++)
             {
